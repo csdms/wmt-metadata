3bcf491304e4f873111d2d5d506ddb285633bc0d
mcflugen <huttone@colorado.edu>
2016-04-22 14:26:37 -0600

    Added metadata for Roms-Lite.

---
+++ metadata/RomsLite/hooks/pre-stage.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+
+def execute(env):
+    """Perform pre-stage tasks for running a component.
+
+    Parameters
+    ----------
+    env : dict
+      A dict of component parameter values from WMT.
+
+    """
+    env['number_of_steps'] = int(round(float(env['_run_duration']) /
+                                       float(env['time_step'])))
