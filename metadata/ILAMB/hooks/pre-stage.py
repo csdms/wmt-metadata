b1a6768ef74880f74fee7c9fcb0228eb26c0f1c3
Mark Piper <mark.piper@colorado.edu>
2016-11-23 15:02:30 -0700

    Empty the pre-stage hook
    It does nothing now.

--- metadata/ILAMB/hooks/pre-stage.py
+++ metadata/ILAMB/hooks/pre-stage.py
@@ -3,11 +3,6 @@
 import os
 import shutil
 
-from wmt.utils.hook import find_simulation_input_file, yaml_dump
-
-
-file_list = []
-
 
 def execute(env):
     """Perform pre-stage tasks for running a component.
@@ -18,4 +13,4 @@ def execute(env):
       A dict of component parameter values from WMT.
 
     """
-    yaml_dump('_env.yaml', env)
+    pass
