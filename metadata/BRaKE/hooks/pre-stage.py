ecaf39cf9bb4d25ae2b9bf147d6feb8456ed3c83
Mark Piper <mark.piper@colorado.edu>
2017-06-06 15:15:45 -0600

    Add BRaKE metadata

---
+++ metadata/BRaKE/hooks/pre-stage.py
@@ -0,0 +1,25 @@
+"""A hook for modifying parameter values read from the WMT client."""
+
+import os
+import shutil
+
+from wmt.utils.hook import find_simulation_input_file, yaml_dump
+
+
+def execute(env):
+    """Perform pre-stage tasks for running a component.
+
+    Parameters
+    ----------
+    env : dict
+      A dict of component parameter values from WMT.
+
+    """
+    env['time_to_run'] = int(env['_run_duration'])
+    env['suffix'] = 'WMT'
+
+    for fname in file_list:
+        src = find_simulation_input_file(env[fname])
+        shutil.copy(src, os.curdir)
+
+    yaml_dump('_env.yaml', env)
