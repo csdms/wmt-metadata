b328cd53a295e476ee2c1e39538c86274624c05f
Mark Piper <mark.piper@colorado.edu>
2017-08-10 17:21:06 -0600

    Generate ILAMB config file from variables in WMT

--- metadata/ILAMB/hooks/pre-stage.py
+++ metadata/ILAMB/hooks/pre-stage.py
@@ -1,6 +1,7 @@
 """A hook for modifying parameter values read from the WMT client."""
 
 from wmt.utils.hook import yaml_dump
+from permafrost_benchmark_system.file import IlambConfigFile
 
 
 def execute(env):
@@ -12,4 +13,15 @@ def execute(env):
       A dict of component parameter values from WMT.
 
     """
+    cmip5_vars = []
+    for k, v in env.items():
+        if k.startswith('_variable') and v != '':
+            cmip5_vars.append(str(v))
+    f = IlambConfigFile(cmip5_vars)
+    f.setup()
+    f.write()
+
+    # For debugging.
+    env['_sources_file'] = f.sources_file
+    env['_cmip5_vars'] = cmip5_vars
     yaml_dump('_env.yaml', env)
