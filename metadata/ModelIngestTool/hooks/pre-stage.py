ef21983b22fc8e116503a342e9fa0f57e0c084af
Mark Piper <mark.piper@colorado.edu>
2017-10-18 15:23:24 -0600

    Refactor WMT metadata for ingest tools
    I revised the hooks and wmt.yaml files for the model and
    benchmark ingest tools. They now follow the pattern used by the
    analogous files in metadata/ILAMB.

--- metadata/ModelIngestTool/hooks/pre-stage.py
+++ metadata/ModelIngestTool/hooks/pre-stage.py
@@ -12,4 +12,10 @@ def execute(env):
       A dict of component parameter values from WMT.
 
     """
+    file_list = []
+    for k, v in env.items():
+        if k.startswith('_ingest_') and v != 'Off':
+            file_list.append(v.encode('utf-8'))
+    env['ingest_files'] = file_list
+
     yaml_dump('_env.yaml', env)
