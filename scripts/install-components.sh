62caeff6b3c7493cb035a3407cea469eb5858be6
Mark Piper <mark.piper@colorado.edu>
2018-06-11 11:43:31 -0600

    Improve robustness of install script
    * The script can now be run from any location on the filesystem.
    * It only installs metadata for components that are present on the executor.
    * It won't retrieve components from the executor if the config file exists.

--- scripts/install-components.sh
+++ scripts/install-components.sh
@@ -1,12 +1,16 @@
 #! /usr/bin/env bash
 # Install all components to the db/components directory of a WMT server.
 
+source="${BASH_SOURCE[0]}"
+source_dir="$(cd -P "$(dirname "$source")" && pwd)"
+project_dir=$(dirname $source_dir)
+
 if [ -z "$wmt_executor" ]; then
     wmt_executor="siwenna.colorado.edu"
 fi
 
 if [ -z "$wmt_executor_username" ]; then
-    wmt_executor=$USER
+    wmt_executor_username="wmtuser"
 fi
 
 if [ -z "$wmt_executor_path" ]; then
@@ -17,20 +21,25 @@ if [ -z "$wmt_server_path" ]; then
     wmt_server_path="/data/web/htdocs/wmt/api/_testing"
 fi
 
-project_dir=$(pwd)
-
 echo "WMT executor      = $wmt_executor"
 echo "WMT executor user = $wmt_executor_username"
 echo "WMT executor path = $wmt_executor_path"
 echo "WMT server path   = $wmt_server_path"
+echo "project path      = $project_dir"
 
-ssh $wmt_executor_username@$wmt_executor \
-    PATH=$wmt_executor_path/conda/bin:\$PATH \
-    cmt-config > $project_dir/wmt-config-$wmt_executor.yaml
+if [ ! -f $project_dir/wmt-config-$wmt_executor.yaml ]; then
+    ssh $wmt_executor_username@$wmt_executor \
+	PATH=$wmt_executor_path/conda/bin:\$PATH \
+	cmt-config > $project_dir/wmt-config-$wmt_executor.yaml
+fi
 
-sudo rm -rf $wmt_server_path/db/components
-sudo cp -r $project_dir/metadata/ $wmt_server_path/db/components
-sudo chown -R $USER $wmt_server_path/db/components
+components=$(python $project_dir/scripts/list-components.py)
+
+rm -rf $wmt_server_path/db/components/*
+for c in $components; do
+    echo "- $c"
+    cp -r $project_dir/metadata/$c $wmt_server_path/db/components
+done
 
 $project_dir/scripts/build-metadata \
     $project_dir/wmt-config-$wmt_executor.yaml \
