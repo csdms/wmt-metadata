b97a0e2c2e127a623ae5af5dd28bfb5951c020da
Mark Piper <mark.piper@colorado.edu>
2018-06-18 12:31:05 -0600

    Start refresh hook; it gets files from executor

---
+++ metadata/ILAMB/hooks/refresh.py
@@ -0,0 +1,38 @@
+"""A hook for refreshing a component's metadata."""
+
+from wmt.utils.ssh import get_host_info, open_connection_to_host
+from wmt.utils.hook import yaml_dump
+
+
+hostname = 'siwenna.colorado.edu'
+pbs_dir = '/home/csdms/ilamb/MODELS-by-project/PBS'
+
+
+# Note that I modified info.json to add login credentials.
+def get_pbs_listing():
+    info = get_host_info(hostname)
+    cmd = 'ls {}'.format(pbs_dir)
+    ssh = open_connection_to_host(info['name'],
+                                  info['username'],
+                                  password=info['password'])
+    _, stdout, _ = ssh.exec_command(cmd)
+    file_list = stdout.readlines()
+    ssh.close()
+    for i, item in enumerate(file_list):
+        file_list[i] = item.rstrip()
+    return file_list
+
+
+def execute(name):
+    """Hook called by components/refresh API.
+
+    Parameters
+    ----------
+    name : str
+      The name of the component.
+
+    """
+
+    # Get files listed in MODELS-by-project/PBS directory.
+    pbs_files = get_pbs_listing()
+    yaml_dump('/tmp/files.yaml', pbs_files)
