2c028a66ec49137296c48002d64a9b5d048c03b2
Mark Piper <mark.piper@colorado.edu>
2018-07-06 16:58:33 -0600

    Refactor ILAMB refresh hook
    The code has been moved to the pbs_server package. See https://github.com/permamodel/pbs-server/pull/3.

--- metadata/ILAMB/hooks/refresh.py
+++ metadata/ILAMB/hooks/refresh.py
@@ -5,19 +5,24 @@ import subprocess
 import json
 from wmt.utils.ssh import get_host_info, open_connection_to_host
 from wmt.config import site
+from pbs_server.models import get_model_name, update_parameters
 
 
 hostname = 'siwenna.colorado.edu'
 pbs_dir = '/home/csdms/ilamb/MODELS-by-project/PBS'
-model_template = { "group": { "name": "pbs_models_group", "members":
-    1, "leader": False }, "name": "{model_name}", "global": False,
-    "value": { "default": "Off", "type": "choice", "choices": [ "On",
-    "Off" ] }, "visible": True, "key": "_model_{model_name}",
-    "description": "{model_name}" }
 
 
 # Note that I modified info.json to add login credentials.
 def get_pbs_listing():
+    """
+    Get a listing of model outputs uploaded through the PBS.
+
+    Returns
+    -------
+    list
+      A list of uploaded model output files.
+
+    """
     info = get_host_info(hostname)
     cmd = 'ls {}'.format(pbs_dir)
     ssh = open_connection_to_host(info['name'],
@@ -29,43 +34,6 @@ def get_pbs_listing():
     for i, item in enumerate(file_list):
         file_list[i] = item.rstrip()
     return file_list
-
-
-def get_model_names(pbs_files):
-    names = []
-    for pbs_file in pbs_files:
-        parts = pbs_file.split('_')
-        name = parts[2]
-        if name not in names:
-            names.append(name)
-    return names
-
-
-def key_in_pbs_group(key, parameters):
-    for param in parameters:
-        if 'group' in param.keys():
-            if param['group']['name'] == 'pbs_models_group':
-                if param['key'] == key:
-                    return True
-    return False
-
-
-def update_parameters(parameters, models):
-    for index, param in enumerate(parameters):
-        if param['key'] == '_pbs_models':
-            pbs_group_header = param
-            pbs_group_index = index
-
-    for model in models:
-        key = '_model_{}'.format(model)
-        if not key_in_pbs_group(key, parameters):
-            entry = model_template.copy()
-            entry['key'] = key
-            entry['name'] = model
-            entry['description'] = model
-            pbs_group_header['group']['members'] += 1
-            pbs_group_index += 1
-            parameters.insert(pbs_group_index, entry)
 
 
 def execute(name):
@@ -82,7 +50,11 @@ def execute(name):
     pbs_files = get_pbs_listing()
 
     # Extract model names from file list, removing duplicates.
-    models = get_model_names(pbs_files)
+    models = []
+    for pbs_file in pbs_files:
+        model_name = get_model_name(pbs_file)
+        if model_name not in models:
+            models.append(model_name)
     models.sort()
 
     # Read the ILAMB parameters.json file.
