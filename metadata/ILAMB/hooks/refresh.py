941cfc4993c4e426b7e2f055052a2dd20ad80a4e
Mark Piper <mark.piper@colorado.edu>
2018-07-18 10:56:08 -0600

    Refactor execute function to improve readability

--- metadata/ILAMB/hooks/refresh.py
+++ metadata/ILAMB/hooks/refresh.py
@@ -43,27 +43,38 @@ def get_pbs_listing(pbs_dir):
     return file_list
 
 
-def execute(name):
-    """Hook called by components/refresh API.
+def update_models(params):
+    """Updates ILAMB metadata for model outputs uploaded through the PBS.
 
     Parameters
     ----------
-    name : str
-      The name of the component.
+    params : list
+      The WMT parameters for the ILAMB component.
 
     """
-
-    # Get names of files uploaded through the PBS.
     models_files = get_pbs_listing(models_dir)
-    data_files = get_pbs_listing(data_dir)
 
-    # Extract model names from file list, removing duplicates.
+    # Extract model names from file list and sort, removing duplicates.
     model_list = []
     for pbs_file in models_files:
         model_name = models.get_name(pbs_file)
         if model_name not in model_list:
             model_list.append(model_name)
     model_list.sort()
+
+    models.update_parameters(params, model_list)
+
+
+def update_variables(params):
+    """Updates ILAMB metadata for benchmark data uploaded through the PBS.
+
+    Parameters
+    ----------
+    params : list
+      The WMT parameters for the ILAMB component.
+
+    """
+    data_files = get_pbs_listing(data_dir)
 
     # Extract variable names from file list, removing duplicates.
     variable_dict = {}
@@ -72,26 +83,35 @@ def execute(name):
         if variable_name not in variable_dict.keys():
             variable_dict[variable_name] = pbs_file
 
-    # Read the ILAMB parameters.json file.
-    parameters_file = os.path.join(site['db'], 'components', name,
-                                   'db', 'parameters.json')
-    with open(parameters_file, 'r') as fp:
-        params = json.load(fp)
-
-    # Add new models and variables to the ILAMB metadata.
-    models.update_parameters(params, model_list)
     variables.update_parameters(params, variable_dict.keys())
-
-    # Write the updated ILAMB parameters.json file.
-    # Note that I had to give `a+w` permissions to the file.
-    with open(parameters_file, 'w') as fp:
-        json.dump(params, fp, indent=4)
 
     # Create or update the .cfg.tmpl file for each variable.
     for var_name, file_name in variable_dict.items():
         variables.update_template(var_name, file_name)
 
-    # Touch the wsgi script so the client reads the changes.
+
+def execute(name):
+    """Hook called by components/refresh API.
+
+    Parameters
+    ----------
+    name : str
+      The name of the component (here, 'ILAMB').
+
+    """
+    parameters_file = os.path.join(site['db'], 'components', name,
+                                   'db', 'parameters.json')
+    with open(parameters_file, 'r') as fp:
+        params = json.load(fp)
+
+    update_models(params)
+    update_variables(params)
+
+    # Note that I had to give `a+w` permissions to the parameters file.
+    with open(parameters_file, 'w') as fp:
+        json.dump(params, fp, indent=4)
+
+    # Touch the wsgi script so the WMT client reads the changes.
     # I had to change the permissions to `a+w` on the wsgi script.
     # And also add site['bin'].
     script_path = os.path.join(site['bin'], 'wmt_wsgi_main.py')
