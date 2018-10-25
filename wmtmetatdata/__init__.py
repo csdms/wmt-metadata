99dfe6fc1c8688b9d1e01218830738f0190c92f1
Mark Piper <mark.piper@colorado.edu>
2018-10-25 16:17:41 -0600

    Add variables for top and metadata directories

---
+++ wmtmetatdata/__init__.py
@@ -0,0 +1,7 @@
+"""Metadata and tools for WMT components."""
+
+import os
+
+
+top_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+metadata_dir = os.path.join(top_dir, 'metadata')
