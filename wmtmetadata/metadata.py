c1c2ae5ede780a50706bdac52919f8d82c17f028
Mark Piper <mark.piper@colorado.edu>
2018-10-28 21:41:48 -0600

    Implement Parameters class

--- wmtmetadata/metadata.py
+++ wmtmetadata/metadata.py
@@ -4,6 +4,7 @@ import os
 import re
 import fnmatch
 import warnings
+import collections
 import json
 import yaml
 from wmtmetadata.utils import commonpath
@@ -35,6 +36,8 @@ class MetadataBase(object):
     def write(self):
         with open(self.filename, 'w') as fp:
             json.dump(self.data, fp, indent=self.indent)
+        if self.filename == 'parameters.json':
+            os.chmod(self.filename, 0666)
 
 
 class Files(MetadataBase):
@@ -108,3 +111,142 @@ class Provides(Ports):
         super(Provides, self).__init__(component)
         self.filename = 'provides.json'
         self.load()
+
+
+def section_break(title):
+    return {
+        'key': 'separator',
+        'name': title,
+        'description': title,
+        'value': {
+            'type': 'string',
+            'default': '',
+        }
+    }
+
+
+def old_style_parameter(name, param):
+    return {
+        'key': name,
+        'name': param['description'],
+        'description': param['description'],
+        'value': param['value'],
+        'visible': param.get('visible', True),
+        'global': param.get('global', False)
+    }
+
+
+class Parameters(MetadataBase):
+
+    def __init__(self, component):
+        super(Parameters, self).__init__(component)
+        self.filename = 'parameters.json'
+        self.data = []
+        self.load_component_config()
+
+        if self.component_config.has_key('extras'):
+            self.parameters.update(self.component_config['extras'])
+
+        if self.component_config.has_key('ignore'):
+            self._hide_ignored()
+
+        if self.component_config.has_key('globals'):
+            driver = section_break('Globals')
+            driver['global'] = True
+            self.data.append(driver)
+            for name in self.component_config.get('globals', []):
+                if name in self.parameters.keys():
+                    self.parameters[name]['global'] = True
+
+        self.make_sections()
+        self.make_groups()
+        self.make_mappers()
+
+    def _hide_ignored(self):
+        ignored = []
+        for pattern in self.component_config['ignore']:
+            p = re.compile(fnmatch.translate(pattern))
+            ignored.extend(filter(p.match, params))
+
+        for name in ignored:
+            if name in self.parameters:
+                self.parameters[name]['visible'] = False
+            else:
+                if name not in self.parameters.keys():
+                    msg = '{name}: pattern not in params'.format(name)
+                    warnings.warn(msg)
+
+    def make_sections(self):
+        added = set()
+
+        for name in self.parameters.keys():
+            if self.parameters[name].has_key('global') and \
+               self.parameters[name]['global'] is True:
+                try:
+                    self.data.append(
+                        old_style_parameter(name, self.parameters[name]))
+                except KeyError:
+                    warnings.warn('{}: missing parameter'.format(name))
+                else:
+                    added.add(name)
+
+        for section in self.component_config.get('sections', []):
+            self.data.append(section_break(section['title']))
+            for member in section['members']:
+                try:
+                    self.data.append(
+                        old_style_parameter(member, self.parameters[member]))
+                except KeyError:
+                    warnings.warn('{}: missing parameter'.format(name))
+                else:
+                    added.add(member)
+
+        missing = set(self.parameters.keys()) - added
+        if len(missing) > 0:
+            for name in missing:
+                self.parameters[name]['visible'] = False
+                self.data.append(
+                    old_style_parameter(name, self.parameters[name]))
+
+    def make_groups(self):
+        groups = self.component_config.get('groups', {})
+        for name, members in groups.items():
+            for item in self.data:
+                if item['key'] in members:
+                    item['group'] = {
+                        "name": name,
+                        "leader": item['key'] == members[0],
+                        "members": len(members),
+                    }
+
+    def _get_mapped_parameters(self, roles):
+        mapped_params = [roles['selector']]
+        for mapping in roles['selections']:
+            param = mapping.values()[0]
+            try:
+                mapped_params.index(param)
+            except ValueError:
+                mapped_params.append(param)
+        return mapped_params
+
+    def _get_mappings(self, selections):
+        mappings = collections.OrderedDict()
+        for mapping in selections:
+            for ptype, param in mapping.items():
+                mappings[ptype] = param
+        return mappings
+
+    def make_mappers(self):
+        mappers = self.component_config.get('parameter_mappers', {})
+        for mapper, roles in mappers.items():
+            mapped_params = self._get_mapped_parameters(roles)
+            for item in self.data:
+                if item['key'] in mapped_params:
+                    item['selection'] = {
+                        'name': mapper,
+                        'selector': roles['selector'] == item['key'],
+                        'members': len(mapped_params),
+                        }
+                    if item['selection']['selector'] is True:
+                        item['selection']['mapping'] = \
+                            self._get_mappings(roles['selections'])
