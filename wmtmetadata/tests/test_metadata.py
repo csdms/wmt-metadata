e4ab8927f661c76566d3e50ca0d8c6e6792dcfeb
Mark Piper <mark.piper@colorado.edu>
2018-10-29 11:10:44 -0600

    Make unit tests for Parameters class
    It exactly replicates the Hydrotrend **parameters.json** file created
    by the old `build-metameta` script.

--- wmtmetadata/tests/test_metadata.py
+++ wmtmetadata/tests/test_metadata.py
@@ -1,19 +1,23 @@
 """Tests for classes in the metadata module."""
 
 import os
+import filecmp
 from wmtmetadata.config import ConfigFromFile
-from wmtmetadata.metadata import Files, Info, Uses, Provides
+from wmtmetadata.metadata import (Files, Info, Uses, Provides,
+                                  Parameters)
 from . import data_dir
 
 
 host = 'siwenna.colorado.edu'
-test_config_file = os.path.join(data_dir, 'wmt-config-siwenna.yaml')
+known_config_file = os.path.join(data_dir, 'wmt-config-siwenna.yaml')
+known_parameters_file = os.path.join(data_dir, 'parameters.json')
 name = 'Hydrotrend'
 files = {
     'files_file': 'files.json',
     'info_file': 'info.json',
     'uses_file': 'uses.json',
     'provides_file': 'provides.json',
+    'parameters_file': 'parameters.json',
     }
 
 
@@ -24,14 +28,14 @@ def teardown_module():
 
 
 def test_files():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     f = Files(config.components[name])
     assert f.data[0] == 'HYDRO.IN'
 
 
 def test_files_write():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     f = Files(config.components[name])
     f.write()
@@ -39,14 +43,14 @@ def test_files_write():
 
 
 def test_info():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     n = Info(config.components[name])
     assert n.data['id'] == name
 
 
 def test_info_write():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     n = Info(config.components[name])
     n.write()
@@ -54,14 +58,14 @@ def test_info_write():
 
 
 def test_uses():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     u = Uses(config.components[name])
     assert u.data == []
 
 
 def test_uses_write():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     u = Uses(config.components[name])
     u.write()
@@ -69,15 +73,34 @@ def test_uses_write():
 
 
 def test_provides():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     u = Provides(config.components[name])
     assert u.data[0]['id'] == 'discharge'
 
 
 def test_provides_write():
-    config = ConfigFromFile(test_config_file)
+    config = ConfigFromFile(known_config_file)
     config.load()
     u = Provides(config.components[name])
     u.write()
     assert os.path.isfile(files['provides_file'])
+
+
+def test_parameters():
+    config = ConfigFromFile(known_config_file)
+    config.load()
+    p = Parameters(config.components[name])
+    assert type(p.data[0]) is dict
+
+
+def test_parameters_write():
+    config = ConfigFromFile(known_config_file)
+    config.load()
+    p = Parameters(config.components[name])
+    p.write()
+    assert os.path.isfile(files['parameters_file'])
+
+
+def test_parameters_file():
+    assert filecmp.cmp(files['parameters_file'], known_parameters_file)
